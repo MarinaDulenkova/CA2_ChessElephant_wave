8b6a5d02cf66fc36c9a9bb8977c249e0a0e06e8f
MarinaDulenkova <mdul97@mail.ru>
2018-12-28 01:25:10 +0300

    class FieldCell added

---
+++ CA2_ElephantMovements_wave/FieldCell.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA2_ElephantMovements_wave
+{
+    /// <summary>
+    /// класс ячейки для шахматной доски
+    /// </summary>
+    public class FieldCell
+    {
+        /// <summary>
+        /// множество состояний ячейки
+        /// </summary>
+        public enum FieldCellType { EMPTY, START, WALL, FINISH };
+
+        /// <summary>
+        /// состояние ячейки и метод аксессор
+        /// </summary>
+        private FieldCellType cellType;
+
+        public FieldCellType CellType
+        {
+            get
+            {
+                return cellType;
+            }
+            set
+            {
+                cellType = value;
+            }
+        }
+
+        /// <summary>
+        /// шаг для посещения и метод аксессор
+        /// </summary>
+        private int step;
+
+        public int Step
+        {
+            get
+            {
+                return step;
+            }
+            set
+            {
+                step = value;
+            }
+        }
+
+        /// <summary>
+        /// посещалась ли ячейка и метод аксессор
+        /// </summary>
+        private bool isVisited;
+
+        public bool IsVisited
+        {
+            get
+            {
+                return isVisited;
+            }
+            set
+            {
+                isVisited = value;
+            }
+        }
+
+        /// <summary>
+        /// конструктор
+        /// </summary>
+        public FieldCell()
+        {
+            cellType = FieldCellType.EMPTY;
+            step = 0;
+            isVisited = false;
+        }
+    }
+}
