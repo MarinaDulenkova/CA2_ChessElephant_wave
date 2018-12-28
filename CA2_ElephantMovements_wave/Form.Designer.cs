43b4a7021d54323ee47361151a39064030d80592
MarinaDulenkova <mdul97@mail.ru>
2018-12-28 04:21:07 +0300

    form Form1 renamed to Form and updated

---
+++ CA2_ElephantMovements_wave/Form.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace CA2_ElephantMovements_wave
+{
+    partial class fmMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbField = new System.Windows.Forms.PictureBox();
+            this.gbControls = new System.Windows.Forms.GroupBox();
+            this.lblAdd = new System.Windows.Forms.Label();
+            this.cbCellType = new System.Windows.Forms.ComboBox();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnRun = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbField)).BeginInit();
+            this.gbControls.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pbField
+            // 
+            this.pbField.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.pbField.Location = new System.Drawing.Point(12, 12);
+            this.pbField.Name = "pbField";
+            this.pbField.Size = new System.Drawing.Size(365, 365);
+            this.pbField.TabIndex = 0;
+            this.pbField.TabStop = false;
+            this.pbField.Click += new System.EventHandler(this.pbField_Click);
+            this.pbField.Paint += new System.Windows.Forms.PaintEventHandler(this.pbField_Paint);
+            // 
+            // gbControls
+            // 
+            this.gbControls.Controls.Add(this.lblAdd);
+            this.gbControls.Controls.Add(this.cbCellType);
+            this.gbControls.Controls.Add(this.btnStop);
+            this.gbControls.Controls.Add(this.btnRun);
+            this.gbControls.Location = new System.Drawing.Point(386, 12);
+            this.gbControls.Name = "gbControls";
+            this.gbControls.Size = new System.Drawing.Size(177, 365);
+            this.gbControls.TabIndex = 1;
+            this.gbControls.TabStop = false;
+            this.gbControls.Text = "Controls:";
+            // 
+            // lblAdd
+            // 
+            this.lblAdd.AutoSize = true;
+            this.lblAdd.Location = new System.Drawing.Point(6, 46);
+            this.lblAdd.Name = "lblAdd";
+            this.lblAdd.Size = new System.Drawing.Size(29, 13);
+            this.lblAdd.TabIndex = 3;
+            this.lblAdd.Text = "Add:";
+            // 
+            // cbCellType
+            // 
+            this.cbCellType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbCellType.FormattingEnabled = true;
+            this.cbCellType.Items.AddRange(new object[] {
+            "Elephant",
+            "Wall",
+            "Exit cell"});
+            this.cbCellType.Location = new System.Drawing.Point(41, 38);
+            this.cbCellType.Name = "cbCellType";
+            this.cbCellType.Size = new System.Drawing.Size(130, 21);
+            this.cbCellType.TabIndex = 2;
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(6, 163);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(165, 45);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "STOP";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnRun
+            // 
+            this.btnRun.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnRun.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnRun.Location = new System.Drawing.Point(6, 110);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(165, 45);
+            this.btnRun.TabIndex = 0;
+            this.btnRun.Text = "RUN";
+            this.btnRun.UseVisualStyleBackColor = false;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // fmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(575, 389);
+            this.Controls.Add(this.gbControls);
+            this.Controls.Add(this.pbField);
+            this.MaximizeBox = false;
+            this.Name = "fmMain";
+            this.Text = "Elephant\'s shortest way: wave algorythm";
+            ((System.ComponentModel.ISupportInitialize)(this.pbField)).EndInit();
+            this.gbControls.ResumeLayout(false);
+            this.gbControls.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbField;
+        private System.Windows.Forms.GroupBox gbControls;
+        private System.Windows.Forms.Label lblAdd;
+        private System.Windows.Forms.ComboBox cbCellType;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnRun;
+    }
+}
+
---
+++ CA2_ElephantMovements_wave/Form.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CA2_ElephantMovements_wave
+{
+    public partial class fmMain : Form
+    {
+        //константы
+        public const int FIELD_SIZE = 8;
+        public const int FIELD_CELL_WIDTH = 40;
+        public const int FIELD_CELL_BORDER_WIDTH = 5;
+        public Color FIELD_CELL_BORDER_COLOR = Color.Black;
+        public Color FIELD_CELL_WALL_COLOR = Color.Red;
+        public Color FIELD_CELL_EMPTY_COLOR = Color.White;
+        public Color FIELD_CELL_START_COLOR = Color.Blue;
+        public Color FIELD_CELL_FINISH_COLOR = Color.Green;
+        public Color FIELD_CELL_PATH_COLOR = Color.Aqua;
+        /// <summary>
+        /// поле
+        /// </summary>
+        public Field field = new Field(FIELD_SIZE);
+        /// <summary>
+        /// поток выполнения под волновой алгоритм
+        /// </summary>
+        private Thread solverThread;
+        /// <summary>
+        /// путь для отображения
+        /// </summary>
+        private List<PointWithStep> path;
+
+        /// <summary>
+        /// конструктор
+        /// </summary>
+        public fmMain()
+        {
+            InitializeComponent();
+            cbCellType.SelectedIndex = 0;
+        }
+
+        /// <summary>
+        /// обработчик нажатия на поле
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void pbField_Click(object sender, EventArgs e)
+        {
+            MouseEventArgs mea = (MouseEventArgs)e;
+            //определяемся с ячейкой и вообще ячейка ли это
+            int cellX = mea.X / (FIELD_CELL_WIDTH + FIELD_CELL_BORDER_WIDTH);
+            int cellY = mea.Y / (FIELD_CELL_WIDTH + FIELD_CELL_BORDER_WIDTH);
+            int x = mea.X % (FIELD_CELL_WIDTH + FIELD_CELL_BORDER_WIDTH);
+            int y = mea.Y % (FIELD_CELL_WIDTH + FIELD_CELL_BORDER_WIDTH);
+            if (x < FIELD_CELL_BORDER_WIDTH)
+            {
+                return;
+            }
+            if (y < FIELD_CELL_BORDER_WIDTH)
+            {
+                return;
+            }
+
+            //разбираемся с кнопкой мыши и устанавливаем нужный тип ячейки
+            if (mea.Button == MouseButtons.Right)
+            {
+                field.GetField()[cellX, cellY].CellType = FieldCell.FieldCellType.EMPTY;
+            }
+            else if (mea.Button == MouseButtons.Left)
+            {
+                field.GetField()[cellX, cellY].CellType = (FieldCell.FieldCellType)(cbCellType.SelectedIndex + 1);
+            }
+            path = null;
+            pbField.Invalidate();
+        }
+
+        /// <summary>
+        /// перерисовка поля
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void pbField_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics graphics = e.Graphics;
+            graphics.Clear(FIELD_CELL_BORDER_COLOR);
+            FieldCell[,] field = this.field.GetField();
+            //рисуем поле нужными цветами
+            for (int i = 0; i < FIELD_SIZE; ++i)
+            {
+                for (int j = 0; j < FIELD_SIZE; ++j)
+                {
+                    Color color = Color.White;
+                    switch (field[i, j].CellType)
+                    {
+                        case FieldCell.FieldCellType.START:
+                            {
+                                color = FIELD_CELL_START_COLOR;
+                                break;
+                            }
+                        case FieldCell.FieldCellType.EMPTY:
+                            {
+                                color = FIELD_CELL_EMPTY_COLOR;
+                                break;
+                            }
+                        case FieldCell.FieldCellType.WALL:
+                            {
+                                color = FIELD_CELL_WALL_COLOR;
+                                break;
+                            }
+                        case FieldCell.FieldCellType.FINISH:
+                            {
+                                color = FIELD_CELL_FINISH_COLOR;
+                                break;
+                            }
+                    }
+                    graphics.FillRectangle(new SolidBrush(color), new Rectangle((i + 1) * FIELD_CELL_BORDER_WIDTH + i * FIELD_CELL_WIDTH, (j + 1) * FIELD_CELL_BORDER_WIDTH + j * FIELD_CELL_WIDTH, FIELD_CELL_WIDTH, FIELD_CELL_WIDTH));
+                }
+            }
+            //рисуем путь, если имеется
+            if (path != null)
+            {
+                foreach (PointWithStep p in path)
+                {
+                    graphics.FillRectangle(new SolidBrush(FIELD_CELL_PATH_COLOR), new Rectangle((p.X + 1) * FIELD_CELL_BORDER_WIDTH + p.X * FIELD_CELL_WIDTH, (p.Y + 1) * FIELD_CELL_BORDER_WIDTH + p.Y * FIELD_CELL_WIDTH, FIELD_CELL_WIDTH, FIELD_CELL_WIDTH));
+                }
+            }
+        }
+
+        /// <summary>
+        /// запуск волнового алгоритма
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            if (!field.IsValid)
+            {
+                MessageBox.Show("На поле должен быть ровно один слон и ровно одна точка выхода!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+            }
+            else
+            {
+                WaveSolver solver = new WaveSolver(field);
+                solver.OnFinished += OnSolverFinishedHandler;
+                solverThread = new Thread(solver.Start);
+                solverThread.Start();
+                btnRun.Enabled = false;
+                btnStop.Enabled = true;
+                Thread.Sleep(0);
+            }
+        }
+
+        /// <summary>
+        /// остановка волнового алгоритма
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            solverThread?.Abort();
+            btnRun.Enabled = true;
+            btnStop.Enabled = false;
+        }
+
+        private delegate void OnSolverFinishedHandlerDelegate(object sender, WaveSolverEventArgs e);
+
+        /// <summary>
+        /// обработка завершения работы волнового алгоритма
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnSolverFinishedHandler(object sender, WaveSolverEventArgs e)
+        {
+            //если не в потоке формы, то разбудить его
+            if (this.InvokeRequired)
+            {
+                Delegate d = new OnSolverFinishedHandlerDelegate(OnSolverFinishedHandler);
+                this.Invoke(d, new object[] { sender, e });
+            }
+            else
+            {
+                //иначе выводим сообщения, чистим потоки, выводим путь, перезапускаем кнопки
+                String message = e.IsPathFound ? "Путь найден!" : "Найти путь не удалось!";
+                MessageBox.Show(message, "Результат", MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1);
+                if (e.IsPathFound)
+                {
+                    path = e.Path;
+                    pbField.Invalidate();
+                }
+                solverThread = null;
+                btnRun.Enabled = true;
+                btnStop.Enabled = false;
+            }
+        }
+    }
+}
